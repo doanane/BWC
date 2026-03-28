bd5d784ceee2715f810f20ac92acd86b4dd6c88a
doanane <anane365221@gmail.com>
2026-03-28 13:00:19 +0000

    Add frontend/birth-and-death-fyp/src/pages/index.js

---
+++ frontend/birth-and-death-fyp/src/pages/index.js
@@ -0,0 +1,9 @@
+// Barrel file for page components (used for non-lazy imports if needed)
+export { default as Home }              from './Home'
+export { default as About }             from './About'
+export { default as Dashboard }         from './Dashboard'
+export { default as SignIn }            from './SignIn'
+export { default as SignUp }            from './SignUp'
+export { default as BirthRegistration } from './BirthRegistration'
+export { default as DeathRegistration } from './DeathRegistration'
+export { default as TrackApplication }  from './TrackApplication'
